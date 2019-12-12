a77c4826d9a79f58aa8ef9e850f86c59fabdc971
Alex Merced - AlexMercedCoder.com <alexmerced@alexmerced.com>
2019-12-12 09:40:36 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import React from 'react';
 import logo from './logo.svg';
+import { Route, Link, BrowserRouter as Router, Switch } from 'react-router-dom'
 import './App.css';
+import Login from './Login.js'
+import Welcome from './Welcome.js'
 
-function App() {
+const App = () => {
+
+    const[state,setState] = React.useState({
+        login: false,
+        username: '',
+    })
+
+    const startUp = state.login ? <Welcome name={state.username}/> : <Login />;
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+        <Header/>
+        {startUp}
+
+
+    <Switch>
+
+
+    </Switch>
+
+    </Router>
     </div>
   );
 }
